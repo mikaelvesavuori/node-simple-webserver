1258d915cb8c5c1a8436ba180ef3e4d5da3c8d32
Mikael Vesavuori <mikael.vesavuori@humblebee.se>
2020-01-15 21:09:13 +0100

    Add default port binding to process.env.PORT else 8080; Add exported vars to build-aws.sh to make it reusable; Bump Fastify version; Update package.json vanity stuff

--- build-aws.sh
+++ build-aws.sh
@@ -1,7 +1,11 @@
-aws ecr get-login --no-include-email --region {REGION} | /bin/bash
+export REGION='eu-north1' # EDIT TO YOUR VALUE
+export PROJECT_NAME='someproject' # EDIT TO YOUR VALUE
+export ACCOUNT_NUMBER='123412341234' # EDIT TO YOUR VALUE
+
+aws ecr get-login --no-include-email --region ${REGION} | /bin/bash
 cd src
 npm install
 cd ..
-docker build -t {PROJECT_NAME} .
-docker tag {PROJECT_NAME}:latest {ACCOUNT_NUMBER}.dkr.ecr.{REGION}.amazonaws.com/{PROJECT_NAME}:latest
-docker push {ACCOUNT_NUMBER}.dkr.ecr.{REGION}.amazonaws.com/{PROJECT_NAME}:latest
+docker build -t ${PROJECT_NAME} .
+docker tag ${PROJECT_NAME}:latest ${ACCOUNT_NUMBER}.dkr.ecr.${REGION}.amazonaws.com/${PROJECT_NAME}:latest
+docker push ${ACCOUNT_NUMBER}.dkr.ecr.${REGION}.amazonaws.com/${PROJECT_NAME}:latest
