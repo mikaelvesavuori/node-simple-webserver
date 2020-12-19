aef8175e54530a666a86ae3aa297d557f8e4bf66
Mikael Vesavuori <mikaelvesavuori@gmail.com>
2020-12-19 16:41:07 +0100

    Add new syntax for GCP Cloud Run deployment

--- build-gcp.sh
+++ build-gcp.sh
@@ -1,5 +1,10 @@
 # EDIT THE BELOW TO YOUR VALUES
 export PROJECT_ID='my_project_id'
 export IMAGE='nodesimplewebserver'
+export REGION='europe-north1'
 
-gcloud builds submit --tag gcr.io/${PROJECT_ID}/${IMAGE}
+# Old syntax
+#gcloud builds submit --tag gcr.io/${PROJECT_ID}/${IMAGE}
+
+# New syntax!
+gcloud beta run deploy $IMAGE --region $REGION --platform managed --source .
